1cc32ada4f874ee65ddae9cda1c0fe74531940e7
Matthias Werner <matthias-werner@fn.de>
2020-04-18 00:46:35 +0200

    added argparser

--- maze_committor.py
+++ maze_committor.py
@@ -3,18 +3,22 @@
 # suppress UPPER_CASE naming style errors
 # pylint: disable=C0103
 
-import sys
+import argparse
 import numpy as np
 import scipy as sp
 from scipy.sparse.linalg import spsolve
 from maze import PerfectMaze
 
+parser = argparse.ArgumentParser(description="Generate N-by-N perfect maze and compute \
+                                the forward committor from bottom left to top right corner")
+parser.add_argument("-N",
+                    metavar="N", default=20,
+                    type=int, help="N-by-N perfect maze (default: 20)")
+
 if __name__ == "__main__":
 
-    if len(sys.argv) > 1:
-        N = int(sys.argv[1])
-    else:
-        N = 20
+    args = parser.parse_args()
+    N = args.N
 
     perfect_maze = PerfectMaze(N)
 
--- maze_solve.py
+++ maze_solve.py
@@ -3,15 +3,18 @@
 # suppress UPPER_CASE naming style errors
 # pylint: disable=C0103
 
-import sys
+import argparse
 from maze import PerfectMaze
+
+parser = argparse.ArgumentParser(description="Generate N-by-N perfect maze and show \
+                                the path from bottom left top right corner")
+parser.add_argument("-N",
+                    metavar="N", default=20,
+                    type=int, help="N-by-N perfect maze (default: 20)")
 
 if __name__ == "__main__":
 
-    if len(sys.argv) > 1:
-        N = int(sys.argv[1])
-    else:
-        N = 20
+    args = parser.parse_args()
 
-    perfect_maze = PerfectMaze(N)
-    perfect_maze.show_path(0, N**2-1)
+    perfect_maze = PerfectMaze(args.N)
+    perfect_maze.show_path(0, args.N**2-1)
